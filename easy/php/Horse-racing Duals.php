1ae9647c9eec48784e5f5b8ff4734dd1b698904f
Lesyuk Serg <seranatles@gmail.com>
2014-12-15 16:34:55 +0200

    Horse Racing Duals Optimizing 2

--- easy/php/Horse-racing Duals.php
+++ easy/php/Horse-racing Duals.php
@@ -1,33 +1,17 @@
 <?php
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 
-fscanf(STDIN, "%d",
-    $N
-);
+fscanf(STDIN, "%d", $N);
 
-$horses = array();
-for ($i = 0; $i < $N; $i++)
-{
-    fscanf(STDIN, "%d",
-        $Pi
-    );
-    $horses[] = $Pi;
+$H = array();
+for ($i = 0; $i < $N; $i++) {
+    fscanf(STDIN, "%d", $H[]);
+}
+sort($H);
+
+$D = array();
+$N--;
+for ($i=0; $i<$N; $i++) {
+    $D[] = abs($H[$i] - $H[$i+1]);
 }
 
-sort($horses);
-$count = count($horses);
-$D = $horses[$count-1]; //max
-
-for ($i=0; $i<$count-1; $i++) {
-    $D_temp = abs($horses[$i] - $horses[$i+1]);
-    
-    if($D > $D_temp) $D = $D_temp;
-}
-
-// Write an action using echo(). DON'T FORGET THE TRAILING \n
-// To debug (equivalent to var_dump): error_log(var_export($var, true));
-
-echo($D."\n");
+echo(min($D)."\n");
