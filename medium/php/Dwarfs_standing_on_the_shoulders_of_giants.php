ddb633620fcfd377af0f4bf54ad2eb7d4577b2d0
Lesyuk Serg <seranatles@gmail.com>
2015-04-29 15:19:23 +0300

    DSOTG optimization

--- medium/php/Dwarfs_standing_on_the_shoulders_of_giants.php
+++ medium/php/Dwarfs_standing_on_the_shoulders_of_giants.php
@@ -1,7 +1,7 @@
 <?php
-    // Read inputs from STDIN. Print outputs to STDOUT.
+// Read inputs from STDIN. Print outputs to STDOUT.
     fscanf(STDIN, "%d", $n);
-    //$t = array(array(8,5),array(3,4),array(2,4),array(2,3),array(1,2));
+//$t = array(array(8,5),array(3,4),array(2,4),array(2,3),array(1,2));
 
     for ($i = 0; $i < $n; $i++) {
         fscanf(STDIN, "%s %s",
@@ -15,35 +15,35 @@
             $TREE[$node1] = array(
                 'nodes' => array($node2),
                 'total' => 0,
-                'flag' => 0
+                'flag'  => 0
             );
         } else {
             $TREE[$node1]['nodes'][] = $node2;
         }
-        
+
         if (!isset($TREE[$node2])) {
             $TREE[$node2] = array(
                 'nodes' => array(),
                 'total' => 0,
-                'flag' => 0
+                'flag'  => 0
             );
         }
     }
 
     error_log(var_export($TREE, true));
     error_log(var_export('---------', true));
-    echo gextri($TREE)+1;
+    echo gextri($TREE) + 1;
     echo "\n";
 
 
-    # ====================================================================
+# ====================================================================
     function gextri($TREE)
-    {   
+    {
         while (!checkedAllNodes($TREE)) {
             $i = startnode($TREE);
             recursewalk($TREE, $i);
         }
-        
+
         return searchMax($TREE);
     }
 
@@ -54,7 +54,7 @@
                 return 0;
             }
         }
-        
+
         return 1;
     }
 
@@ -65,11 +65,11 @@
                 break;
             }
         }
-        
+
         $flag = 1;
         while ($flag) {
             $flag = 0;
-            
+
             foreach ($TREE as $i => $T) {
                 if (in_array($k, $T['nodes'])) {
                     $flag = 1;
@@ -77,27 +77,29 @@
                 }
             }
         }
+
         return $k;
     }
 
     function recursewalk(&$TREE, $i)
     {
         $TREE[$i]['flag'] = 1;
-        foreach ($TREE[$i]['nodes'] as $k=>$r) {
+        foreach ($TREE[$i]['nodes'] as $k => $r) {
             $TREE[$r]['total'] = calcCost($TREE, $i, $k);
-            
+
             recursewalk($TREE, $r);
         }
     }
 
     function calcCost($TREE, $i, $N) # calculate cost for node N
     {
-        $r   = $TREE[$i]['nodes'][$N];
+        $r = $TREE[$i]['nodes'][$N];
         $sum = $TREE[$i]['total'] + 1;
-            
+
         if ($TREE[$r]['total'] < $sum) {
             return $sum;
         }
+
         return $TREE[$r]['total'];
     }
 
@@ -108,6 +110,6 @@
         foreach ($ROOMS as $R) {
             $REZ[] = $R['total'];
         }
-        
+
         return max($REZ);
     }
