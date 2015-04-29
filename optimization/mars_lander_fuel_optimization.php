adf85cfb731f671c88c68bd3394f2271eb55efe7
Lesyuk Serg <seranatles@gmail.com>
2015-04-29 15:16:36 +0300

    mars lander optimization

--- optimization/mars_lander_fuel_optimization.php
+++ optimization/mars_lander_fuel_optimization.php
@@ -14,11 +14,11 @@
             $LAND_X, // X coordinate of a surface point. (0 to 6999)
             $LAND_Y // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
         );
-        $coords[] = array('x'=>$LAND_X, 'y'=>$LAND_Y);
+        $coords[] = array('x' => $LAND_X, 'y' => $LAND_Y);
 
-        if($i > 0 && $coords[$i]['y'] == $coords[$i-1]['y']) {
+        if ($i > 0 && $coords[$i]['y'] == $coords[$i - 1]['y']) {
             $Y_landing = $coords[$i]['y'];
-            $X1_landing = $coords[$i-1]['x'];
+            $X1_landing = $coords[$i - 1]['x'];
             $X2_landing = $coords[$i]['x'];
 
             error_log(var_export($Y_landing, true));
@@ -26,11 +26,10 @@
             error_log(var_export($X2_landing, true));
         }
     }
-    $X_landing = ($X1_landing+$X2_landing)/2;
-    $L_length = $X2_landing-$X1_landing;
+    $X_landing = ($X1_landing + $X2_landing) / 2;
+    $L_length = $X2_landing - $X1_landing;
 
-    while (TRUE)
-    {
+    while (true) {
         $i++;
         fscanf(STDIN, "%d %d %d %d %d %d %d",
             $X,
@@ -43,29 +42,29 @@
         );
 
         error_log(var_export($X_landing - $X, true));
-        if(abs($X_landing - $X) < ($L_length+100)/2 ){
-            if($HS > 10){
+        if (abs($X_landing - $X) < ($L_length + 100) / 2) {
+            if ($HS > 10) {
                 echo("50 4\n");
-            }else if($HS < -10){
+            } else if ($HS < -10) {
                 echo("-50 4\n");
-            }else{
-                if($VS < -39){
+            } else {
+                if ($VS < -39) {
                     echo("0 4\n");
-                }else{
+                } else {
                     echo("0 2\n");
                 }
             }
         } else {
-            if( ($X_landing - $X) > 0){
-                if($HS > 40){
+            if (($X_landing - $X) > 0) {
+                if ($HS > 55) {
                     echo("20 4\n");
-                }else{
+                } else {
                     echo("-20 4\n");
                 }
-            }else if(($X_landing - $X) < 0){
-                if($HS < -40){
+            } else if (($X_landing - $X) < 0) {
+                if ($HS < -55) {
                     echo("-20 4\n");
-                }else{
+                } else {
                     echo("20 4\n");
                 }
             }
