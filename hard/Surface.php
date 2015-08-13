bb1c468d2ca64fac9e07bf09b17df081465f3961
Lesyuk Serg <seranatles@gmail.com>
2015-08-13 17:10:55 +0300

    Surface method2 90%

--- hard/Surface.php
+++ hard/Surface.php
@@ -1,18 +1,17 @@
-<?
+<?php
     $FIELD = [];
-    $line = '';
 
     fscanf(STDIN, "%d", $L); //Length of field
     fscanf(STDIN, "%d", $H); //Height of field
 
     for ($i = 0; $i < $H; $i++) {
         fscanf(STDIN, "%s", $line); // field
-
         $FIELD[] = str_split($line);
-        echo count($FIELD);
     }
 
     fscanf(STDIN, "%d", $N); // Number of coordinates to check
+
+    //error_log(var_export("\n".implode("\n",$FIELD), true));
 
     for ($i = 0; $i < $N; $i++) {
         fscanf(STDIN, "%d %d", $X, $Y);
@@ -21,68 +20,64 @@
 
         // Write an action using echo(). DON'T FORGET THE TRAILING \n
         // To debug (equivalent to var_dump): error_log(var_export($var, true));
-        //echo count($FIELD);
+        error_log(var_export($FIELD[$Y][$X], true));
+
         echo preCalculateLakeSize($FIELD, $Y, $X);
         echo("\n");
-
-        /*foreach ($FIELD as $ROW) {
-            error_log(var_export(implode("", $ROW), true));
-        }
-        error_log(var_export("\n", true));
-        */
     }
-
-    # ===============================================================================
-    # ===============================================================================
-    # ===============================================================================
 
     function preCalculateLakeSize(&$FIELD, $Y, $X)
     {
         if ($FIELD[$Y][$X] == 'O') {
-            $check = [];
-            $total = calculateLakeSize($FIELD, $Y, $X, $check);
-
-            if (count($check)) {
-                foreach ($check as $Y => $XX) {
-                    foreach ($XX as $X => $V) {
-                        $FIELD[$Y][$X] = $total;
-                    }
+            $lake = [];
+            calculateLakeSize($FIELD, $Y, $X, $lake, $size);
+//die('1');
+            foreach ($lake as $Y => $point) {
+                foreach ($point as $X => $V) {
+                    $FIELD[$Y][$X] = $size;
                 }
             }
+
+            return $size;
+
+        } elseif ($FIELD[$Y][$X] == '#') {
+            return 0;
+
         } else {
-            $total = (int)$FIELD[$Y][$X];
+            return $FIELD[$Y][$X];
         }
 
 
-        return $total;
     }
 
-    function calculateLakeSize(&$FIELD, $Y, $X, &$check)
+    function calculateLakeSize(&$FIELD, $Y, $X, &$lake, &$total)
     {
-        $total = 1;
-        $FIELD[$Y][$X] = '1';
-        $check[$Y][$X] = 1;
+        if ($FIELD[$Y][$X] == 'O') {
+            $total++;
+            $lake[$Y][$X] = '0';
+            $FIELD[$Y][$X] = '1';
+        } else {
+            return 0;
+        }
 
 
         if (isset($FIELD[$Y - 1][$X]) && $FIELD[$Y - 1][$X] == 'O') { // UP
             //error_log(var_export("GO UP ".($Y - 1) . "_" . $X, true));
-            $total += calculateLakeSize($FIELD, $Y - 1, $X, $check);
+            calculateLakeSize($FIELD, $Y - 1, $X, $lake, $total);
         }
 
         if (isset($FIELD[$Y + 1][$X]) && $FIELD[$Y + 1][$X] == 'O') { // DOWN
             //error_log(var_export("GO DOWN ".($Y + 1) . "_" . $X, true));
-            $total += calculateLakeSize($FIELD, $Y + 1, $X, $check);
+            calculateLakeSize($FIELD, $Y + 1, $X, $lake, $total);
         }
 
         if (isset($FIELD[$Y][$X - 1]) && $FIELD[$Y][$X - 1] == 'O') { // LEFT
             //error_log(var_export("GO LEFT ".$Y . "_" . ($X - 1), true));
-            $total += calculateLakeSize($FIELD, $Y, $X - 1, $check);
+            calculateLakeSize($FIELD, $Y, $X - 1, $lake, $total);
         }
 
         if (isset($FIELD[$Y][$X + 1]) && $FIELD[$Y][$X + 1] == 'O') { //RIGHT
             //error_log(var_export("GO RIGHT ".$Y . "_" . ($X + 1), true));
-            $total += calculateLakeSize($FIELD, $Y, $X + 1, $check);
+            calculateLakeSize($FIELD, $Y, $X + 1, $lake, $total);
         }
-
-        return $total;
     }
