de6058c8bca90cf16f3c5c33a946d1a028ca8237
Lesyuk Serg <seranatles@gmail.com>
2015-12-10 15:30:33 +0200

    Winamax OOP 100%

--- 02-medium/php/Winamax_Sponsored_Challenge.php
+++ 02-medium/php/Winamax_Sponsored_Challenge.php
@@ -1,154 +1,151 @@
-<?php
-    $deckP1 = $deck2 = [];
+<?
+    //define('STDIN', fopen('input.txt', 'r'));
 
-    /*fscanf(STDIN, "%d", $n); // the number of cards for player 1
-    for ($i = 0; $i < $n; $i++) {
-        fscanf(STDIN, "%s", $deckP1[]); // the n cards of player 1
+    class battle
+    {
+        public $deckP1;
+        public $deckP2;
+        public $rounds;
+        public $cardCost;
+        public $warDeckP1;
+        public $warDeckP2;
+
+        public function __construct()
+        {
+            $this->cardCost = array(2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14);
+
+            fscanf(STDIN, "%d", $n); // the number of cards for player 1
+            for ($i = 0; $i < $n; $i++) {
+                fscanf(STDIN, "%s", $card); // the n cards of player 1
+                $this->deckP1[] = $this->cardCost($card);
+            }
+
+            fscanf(STDIN, "%d", $m); // the number of cards for player 2
+            for ($i = 0; $i < $m; $i++) {
+                fscanf(STDIN, "%s", $card); // the m cards of player 2
+                $this->deckP2[] = $this->cardCost($card);
+            }
+        }
+
+        public function cardCost($card)
+        {
+            if ($card) {
+                $clearCard = str_replace(array('D', 'H', 'C', 'S'), '', $card);
+
+                return $this->cardCost[$clearCard];
+            }
+
+            return 0;
+        }
+
+        public function show_me($text, $deck)
+        {
+            $show = '';
+            foreach ($deck as $card) {
+                if (strlen($card) > 2) {
+                    $show .= $card . ' ';
+                } else {
+                    $show .= ' ' . $card . ' ';
+                }
+            }
+
+            error_log(var_export($text . $show, true));
+        }
+
+        public function start()
+        {
+            while (count($this->deckP1) && count($this->deckP2)) {
+                //$this->show_me('1deck - ', $this->deckP1);
+                //$this->show_me('2deck - ', $this->deckP2);
+                $this->rounds++;
+                $cardP1 = array_shift($this->deckP1);
+                $cardP2 = array_shift($this->deckP2);
+
+                //error_log(var_export("{$cardP1} ? {$cardP2}", true));
+
+                if ($cardP1 > $cardP2) {
+                    //error_log(var_export("win1", true));
+                    array_push($this->deckP1, $cardP1, $cardP2);
+
+                } elseif ($cardP1 < $cardP2) {
+                    //error_log(var_export("win2", true));
+                    array_push($this->deckP2, $cardP1, $cardP2);
+
+                } else {
+                    $this->warDeckP1 = array($cardP1);
+                    $this->warDeckP2 = array($cardP2);
+                    $war = $this->war();
+                    //error_log(var_export('========== END WAR ==================', true));
+
+                    if ($war == 1) {
+                        //error_log(var_export("win1", true));
+                        $this->deckP1 = array_merge($this->deckP1, $this->warDeckP1);
+
+                    } elseif ($war == 2) {
+                        //error_log(var_export("win2", true));
+                        $this->deckP2 = array_merge($this->deckP2, $this->warDeckP2);
+
+                    } else {
+                        return false;
+                    }
+                }
+
+                //error_log(var_export('================================', true));
+            }
+            //error_log(var_export('1deck - ' . implode(' ', $this->deckP1), true));
+            //error_log(var_export('2deck - ' . implode(' ', $this->deckP2), true));
+            if (count($this->deckP1) > count($this->deckP2)) {
+                $player = 1;
+            } else {
+                $player = 2;
+            }
+
+            return $player;
+        }
+
+        public function war()
+        {
+            if (count($this->deckP1) < 4 || count($this->deckP2) < 4) {
+                return 'PAT';
+            }
+
+            //$this->show_me('1deck - ', $this->deckP1);
+            //$this->show_me('2deck - ', $this->deckP2);
+
+            for ($i = 0; $i < 4; $i++) {
+                $this->warDeckP1[] = array_shift($this->deckP1);
+                $this->warDeckP2[] = array_shift($this->deckP2);
+            }
+
+            $lastCard1 = end($this->warDeckP1);
+            $lastCard2 = end($this->warDeckP2);
+
+            //error_log(var_export($lastCard1 . ' ?? ' . $lastCard2, true));
+
+            if ($lastCard1 > $lastCard2) {
+                $this->warDeckP1 = array_merge($this->warDeckP1, $this->warDeckP2);
+
+                return 1;
+
+            } elseif ($lastCard1 < $lastCard2) {
+                $this->warDeckP2 = array_merge($this->warDeckP1, $this->warDeckP2);
+
+                return 2;
+            }
+
+            return $this->war();
+        }
     }
 
-    fscanf(STDIN, "%d", $m); // the number of cards for player 2
-    for ($i = 0; $i < $m; $i++) {
-        fscanf(STDIN, "%s", $deckP2[]); // the m cards of player 2
-    }*/
+    $game = new battle();
 
-    $deckP1 = "4C 4S QS 7D QD AS 6H 5D 2S 10S 3S 2C JS 10C 2D 5H KC AD KD JD JH 9H 7S 6S 3D 8S";
-    $deckP1 = explode(' ',$deckP1);
-
-    $deckP2 = "3H 7C KS 9D 4D 6D 8D JC 9S 10H 5C 8H AC 2H 6C 7H 10D 3C KH AH 9C QC 4H 8C QH 5S";
-    $deckP2 = explode(' ',$deckP2);
-
-    $rez = battle($deckP1, $deckP2);
-    if ($rez['win']) {
-        echo $rez['win'] . ' ' . $rez['rounds'] . "\n";
+    $rez = $game->start();
+    if ($rez) {
+        echo $rez . ' ' . $game->rounds . "\n";
     } else {
         echo "PAT\n";
     }
 
-
-    # ================================================================================
-    # =========== FUNCTIONS ==========================================================
-    # --------------------------------------------------------------------------------
-
-    function show_me($text, $deck) {
-        $show = '';
-        foreach($deck as $card){
-            if(strlen($card) > 2) {
-                $show .= $card.' ';
-            } else {
-                $show .= ' '.$card.' ';
-            }
-        }
-        var_dump($text.$show);
-    }
-
-    function battle($deckP1, $deckP2)
-    {
-        $rounds = 0;
-
-        while (count($deckP1) && count($deckP2)) {
-            var_dump("<<<<< {$rounds} >>>>>>>>>>");
-            show_me('1deck - ',$deckP1);
-            show_me('2deck - ',$deckP2);
-            $rounds++;
-            $cardP1 = array_shift($deckP1);
-            $cardP2 = array_shift($deckP2);
-            var_dump("{$cardP1} ? {$cardP2}");
-
-            $card1 = cardCost($cardP1);
-            $card2 = cardCost($cardP2);
-
-            if ($card1 > $card2) {
-                var_dump("win 1");
-                array_push($deckP1, $cardP1, $cardP2);
-
-            } elseif ($card1 < $card2) {
-                var_dump("win 2");
-                array_push($deckP2, $cardP1, $cardP2);
-
-            } else {
-                $card3P1 = $card3P2 = [];
-                var_dump('============ WAR ====================');
-                $war = war($deckP1, $deckP2, $card3P1, $card3P2);
-                //error_log(var_export($war, true));
-                //error_log(var_export('========== END WAR ==================', true));
-
-                if ($war['win'] == 1) {
-                    var_dump("win 1");
-                    array_push($deckP1, $cardP1, $cardP2);
-                    $deckP1 = array_merge($deckP1, $war['deck']);
-
-                } elseif ($war['win'] == 2) {
-                    var_dump("win 2");
-                    array_push($deckP2, $cardP1, $cardP2);
-                    $deckP2 = array_merge($deckP2, $war['deck']);
-                }
-            }
-
-            //error_log(var_export('================================', true));
-        }
-        //error_log(var_export('1deck - ' . implode(' ', $deckP1), true));
-        //error_log(var_export('2deck - ' . implode(' ', $deckP2), true));
-        if (count($deckP1) > count($deckP2)) {
-            $player = 1;
-        } else {
-            $player = 2;
-        }
-
-        return array('win' => $player, 'rounds' => $rounds);
-    }
-
-    function war(&$deckP1, &$deckP2, $warCardP1, $warCardP2)
-    {
-        show_me('1deck - ',$deckP1);
-        show_me('2deck - ',$deckP2);
-        for ($i = 0; $i < 4; $i++) {
-            if (current($deckP1)) {
-                $warCardP1[] = array_shift($deckP1);
-            }
-        }
-
-        for ($i = 0; $i < 4; $i++) {
-            if (current($deckP2)) {
-                $warCardP2[] = array_shift($deckP2);
-            }
-        }
-
-        $lastCard1 = end($warCardP1);
-        $lastCard2 = end($warCardP2);
-
-        $card1 = cardCost($lastCard1);
-        $card2 = cardCost($lastCard2);
-
-        //error_log(var_export($lastCard1.''.$card1, true));
-        //error_log(var_export($lastCard2.''.$card2, true));
-
-        if ($card1 > $card2) {
-            $warCardP1 = array_merge($warCardP1, $warCardP2);
-
-            return array('win' => 1, 'deck' => $warCardP1);
-
-        } elseif ($card1 < $card2) {
-            $warCardP2 = array_merge($warCardP2, $warCardP1);
-
-            return array('win' => 2, 'deck' => $warCardP2);
-        }
-
-        var_dump("{$lastCard1} ? {$lastCard2}");
-        war($deckP1, $deckP2, $warCardP1, $warCardP2);
-    }
-
-    function cardCost($card)
-    {
-        if ($card) {
-            $cardCost = array(2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14);
-            $replace = array('D', 'H', 'C', 'S');
-            $clearCard = str_replace($replace, '', $card);
-
-            return $cardCost[$clearCard];
-        } else {
-            return 0;
-        }
-    }
 
     // Write an action using echo(). DON'T FORGET THE TRAILING \n
     // To debug (equivalent to var_dump): error_log(var_export($var, true));
