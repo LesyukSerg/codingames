90775fa62785ba67ef30e693c310fd76a7f322c8
Lesyuk Serg <seranatles@gmail.com>
2016-02-02 16:30:06 +0200

    optimization temperatures

--- optimization/Temperatures.php
+++ optimization/Temperatures.php
@@ -1,2 +1,1 @@
-<? fscanf(STDIN,"%d",$N);$T=stream_get_line(STDIN,256,"\n");if($T){$T=explode(' ',$T);$p=9999;$m=-$p;foreach($T as $t){if($t<0){if($m<$t)$m=$t;}elseif($p>$t)$p=$t;}if(-$m < $p)die("$m\n");else die("$p\n");}die("0\n");
-
+<?fscanf(STDIN,"%d",$N);if($N){$m=-$p=999;while($N-->0){$t=(int)stream_get_line(STDIN,9," ");if($t<0){if($m<$t)$m=$t;}elseif($p>$t)$p=$t;}if(-$m<$p)die("$m\n");else die("$p\n");}die("0\n");
