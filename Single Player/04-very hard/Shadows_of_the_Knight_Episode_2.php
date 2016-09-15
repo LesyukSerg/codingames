c27f4dc0cd86a7b0b8ef1b2c8c1cb320f726307d
Lesyuk Serg <seranatles@gmail.com>
2016-09-15 17:03:02 +0300

    second try

--- Single Player/04-very hard/Shadows_of_the_Knight_Episode_2.php
+++ Single Player/04-very hard/Shadows_of_the_Knight_Episode_2.php
@@ -1,5 +1,5 @@
 <?php
-    //define('STDIN', fopen('input.txt', 'r'));
+    define('STDIN', fopen('input.txt', 'r'));
 
     fscanf(STDIN, "%d %d",
         $W, // width of the building.
@@ -12,12 +12,12 @@
     $X_START = $Y_START = 0;
     $X_END = $W;
     $Y_END = $H;
-    $minY = $maxY = 0;
-    $newY = $Y0;
+
     $newX = $X0;
-    $oldX = $oldY = [];
-    $oldX[] = $X0;
-    $oldY[] = $Y0;
+    $newY = $Y0;
+
+    $oldX = array($X0);
+    $oldY = array($Y0);
     // game loop
     $i = 0;
     $bombDir_X = "UNKNOWN";
@@ -30,7 +30,6 @@
         fscanf(STDIN, "%s",
             $bombDir // Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
         );
-        error_log(var_export($bombDir, true));
 
         if ($i % 2 == 1) {
             $bombDir_Y = $bombDir;
@@ -39,39 +38,44 @@
         }
         // Write an action using echo(). DON'T FORGET THE TRAILING \n
         // To debug (equivalent to var_dump): error_log(var_export($var, true));
-        $newY = search($bombDir, $H, $Y0, $Y_START, $Y_END, $oldY, 1);
-        $newX = search($bombDir, $W, $X0, $X_START, $X_END, $oldX, 1);
+
+        change_borders($bombDir_Y, $Y0, $Y_START, $Y_END, $oldY);
+        if ($bombDir_Y == 'COLDER' && $Y_START == $Y0 && $i % 2 == 1) {
+            $Y_START++;
+            $Y0++;
+        }
+
+        change_borders($bombDir_X, $X0, $X_START, $X_END, $oldX);
+        if ($bombDir_Y == 'COLDER' && $X_START == $X0 && $i % 2 == 0) {
+            $X_START++;
+            $X0++;
+        }
+
+        error_log(var_export("Y_START - $Y_START; Y_END - $Y_END | " . "X_START - $X_START; X_END - $X_END", true));
+        error_log(var_export($i, true));
+
+        if ($bombDir == 'UNKNOWN' || $bombDir == 'SAME' || ($i % 2 == 1 && $Y_END - $Y_START == 1) || ($i % 2 == 0 && $X_END - $X_START == 1)) {
+            $i++;
+        }
+
+        if ($i % 2 == 1) {
+            $newY = search($bombDir, $H, $Y0, $Y_START, $Y_END, $oldY);
+            $newX = $X0;
+            $oldY[] = $Y0;
+        } else {
+            $newX = search($bombDir, $W, $X0, $X_START, $X_END, $oldX);
+            $newY = $Y0;
+            $oldX[] = $X0;
+        }
+
+
         if ($newY > $H - 1) $newY--;
         if ($newX > $W - 1) $newX--;
 
-        /*if ($i % 2 == 0) {
-            if ($Y_START != $Y_END - 1) {
-                $newY = search($bombDir_Y, $H, $Y0, $Y_START, $Y_END, $oldY, 0);
-                if ($newX > $H - 1) $newY--;
-            }
-
-            search($bombDir_X, $W, $X0, $X_START, $X_END, $oldX, 1);
-
-            echo "{$X0} {$newY}\n";
-            $oldY[] = $Y0;
-        } else {
-            search($bombDir_Y, $H, $Y0, $Y_START, $Y_END, $oldY, 1);
-
-            if ($X_START != $X_END - 1) {
-                $newX = search($bombDir_X, $W, $X0, $X_START, $X_END, $oldX, 0);
-                if ($newX > $W - 1) $newX--;
-            }
-
-            echo "{$newX} {$Y0}\n";
-            $oldX[] = $X0;
-        }*/
-
         echo "{$newX} {$newY}\n";
-        error_log(var_export("Y_START - $Y_START; Y_END - $Y_END | " . "X_START - $X_START; X_END - $X_END", true));
-        $i++;
     }
 
-    function search($bombDir, $WH, $currentPos, &$START, &$END, &$old, $calc)
+    function search($bombDir, $WH, $currentPos, &$START, &$END, &$old)
     {
         if ($bombDir == 'UNKNOWN') {
             if ($currentPos > ($WH / 2)) {
@@ -84,45 +88,55 @@
 
         } elseif ($bombDir == 'COLDER') {
             if ($currentPos < end($old)) {
-                if ($calc)
-                    $START = end($old) - ceil((end($old) - $currentPos) / 2);
-
                 $step = ceil(($END - $START) / 2);
                 $new = $START + $step;
 
             } else {
-                if ($calc)
-                    $END = end($old) + ceil(($currentPos - end($old)) / 2);
-
                 $step = ceil(($END - $START) / 2);
                 $new = $START + $step;
             }
 
         } elseif ($bombDir == 'WARMER') {
             if ($currentPos < end($old)) {
-                if ($calc) {
-                    $END = end($old) - ceil(($END - $currentPos) / 2);
-                }
-
-
                 $step = -ceil(($currentPos - $START) / 2);
                 $new = $currentPos + $step;
             } else {
-                if ($calc)
-                    $START = end($old) + ceil(($currentPos - end($old)) / 2);
-
                 $step = ceil(($END - $currentPos) / 2);
                 $new = $currentPos + $step;
             }
         } else {
-            if ($calc) {
-                $END = $currentPos + floor(($currentPos - end($old)) / 2);
-                $START = $currentPos;
-            }
-
             $step = ceil(($END - $START) / 2);
             $new = $currentPos + $step;
         }
 
         return $new;
+    }
+
+    function change_borders($bombDir, $currentPos, &$START, &$END, &$old)
+    {
+        if ($END - $START > 1) {
+            if ($bombDir == 'UNKNOWN') {
+
+            } elseif ($bombDir == 'COLDER') {
+                if ($currentPos < end($old)) {
+                    $START = end($old) - ceil((end($old) - $currentPos) / 2);
+
+                } else {
+                    $END = end($old) + ceil(($currentPos - end($old)) / 2);
+                }
+
+            } elseif ($bombDir == 'WARMER') {
+                if ($currentPos < end($old)) {
+                    $END = $currentPos + ceil((end($old) - $currentPos) / 2);
+
+                } else {
+                    $START = end($old) + ceil(($currentPos - end($old)) / 2);
+                }
+            } else {
+                $END = $currentPos + floor(($currentPos - end($old)) / 2);
+                $START = $currentPos;
+            }
+        } else {
+            $END = $START = $currentPos;
+        }
     }
