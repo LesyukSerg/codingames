9981507a8985d0fe3b58e5a5c1f0f44bb88aff55
Serg <seranatles@gmail.com>
2017-12-13 17:31:52 +0200

    improve to 107 symbols

--- Code Golf/Temperatures.php
+++ Code Golf/Temperatures.php
@@ -1,1 +1,1 @@
-<?fgets(STDIN);$t=explode(' ',trim(fgets(STDIN)));sort($t);$m=999;foreach($t as $p)$m=abs($p)<=abs($m)?$p:$m;echo $m?$m:0;
+<?fgets(STDIN);$t=fgetcsv(STDIN,0,' ');sort($t);$m=999;foreach($t as$p)$m=abs($p)<=abs($m)?$p:$m;echo$m?:0;
