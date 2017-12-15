787d92523daf533b7d72717f2cca593dafdee9c9
Serg <seranatles@gmail.com>
2017-12-15 09:55:46 +0200

    improve to 239 symbols

--- Code Golf/Paranoid_Android.php
+++ Code Golf/Paranoid_Android.php
@@ -1,15 +1,1 @@
-<?fscanf(STDIN, "%d %d %d %d %d %d %d %d",$z,$y,$x,$F,$P,$w,$a,$n);
-    $E=[];
-    for($i=0;$i<$n;$i++) {
-        fscanf(STDIN, "%d %d", $eF, $eP);
-        $E[$eF] = $eP;
-    }
-    $E[$F] = $P;
-    $F = '';
-    while (1) {
-        fscanf(STDIN, "%d %d %s", $C, $P, $d);
-        if (($E[$C] < $P && $d == 'RIGHT') || ($E[$C] > $P && $d == 'LEFT'))
-            echo ($F !== $C) ? "BLOCK" : "WAIT";
-        else echo "WAIT";
-        echo "\n";
-    }
+<?$p=fgetcsv(STDIN,0,' ');while($p[7]-->0){fscanf(STDIN,"%d %d",$F,$P);$E[$F]=$P;}$E[$p[3]]=$p[4];while(1){fscanf(STDIN,"%d %d %s",$C,$P,$d);echo(($E[$C]<$P&&$d=='RIGHT')||($E[$C]>$P&&$d=='LEFT'))?(''!==$C)?"BLOCK":"WAIT":"WAIT";echo"\n";}
