f84be43a6dae84815306baee8babac2403dcdb37
Serg <seranatles@gmail.com>
2018-03-28 17:54:20 +0300

    js

---
+++ Code Golf/PowerOfThor.js
@@ -0,0 +1,1 @@
+for([x,y,X,Y]=readline().split(' ');;)print((Y++< y?"S":"")+(X++< x?"E":--X> x?"W":""));
---
+++ Single Player/01-easy/javascript/The_Bridge_Episode_1.js
@@ -0,0 +1,28 @@
+var road = parseInt(readline()); // the length of the road before the gap.
+var gap = parseInt(readline()); // the length of the gap.
+var platform = parseInt(readline()); // the length of the landing platform.
+
+var after_jump = road + gap - 1;
+var before_jump = road;
+
+// game loop
+while (true) {
+    var S = parseInt(readline()); // the motorbike's speed.
+    var X = parseInt(readline()); // the position on the road of the motorbike.
+
+    if (X + S <= before_jump) {
+        if (S <= gap) {
+            print('SPEED');
+        } else if (S > gap + 1) {
+            print('SLOW');
+        } else {
+            print('WAIT');
+        }
+    } else if (X > after_jump) {
+        print('SLOW');
+    } else {
+        print('JUMP');
+    }
+
+    // To debug: printErr('Debug messages...');
+}
