fdf1848a7c2530632276dd0012dd5891bd139679
Serhii <seranatles@gmail.com>
2023-11-30 13:38:22 +0200

    + 1 puzzle

---
+++ Comunity_Puzzles/D01_Easy/Offset_Arrays.php
@@ -0,0 +1,94 @@
+<?php
+
+    namespace Comunity_Puzzles\D01_Easy;
+
+    class newArray
+    {
+        private $name;
+        private $firstKey;
+        private $items;
+
+        public function __construct($line)
+        {
+            preg_match("#([A-Z]+)\[(-?\d+)..(-?\d+)\] = (.+)#", $line, $found);
+
+            $this->name = $found[1];
+            $this->firstKey = $found[2];
+            $this->items = explode(' ', $found[4]);
+        }
+
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        public function getItemByKey($key)
+        {
+            return $this->items[$key - $this->firstKey];
+        }
+    }
+
+    class Result
+    {
+        private string $line;
+        private array $arrays;
+
+        public function __construct($line, $arrays)
+        {
+            $this->line = $line;
+            $this->arrays = $arrays;
+        }
+
+        public function lineReplace($search, $replace)
+        {
+            $this->line = str_replace($search, $replace, $this->line);
+        }
+
+        public function findResult(): string
+        {
+            while (strstr($this->line, '[')) {
+                $this->getOneItem();
+            }
+
+            return $this->line;
+        }
+
+        public function getOneKey(): array
+        {
+            preg_match("#([A-Z]+)\[(-?\d+)\]#", $this->line, $found);
+
+            return [$found[0], $found[1], $found[2]];
+        }
+
+        public function getOneItem()
+        {
+            list($full, $name, $key) = $this->getOneKey();
+
+            $res = $this->arrays[$name]->getItemByKey($key);
+            $this->lineReplace($full, $res);
+
+            return $res;
+        }
+    }
+
+    function start()
+    {
+        $arrays = [];
+        fscanf(STDIN, "%d", $n);
+        for ($i = 0; $i < $n; $i++) {
+            $line = stream_get_line(STDIN, 1024 + 1, "\n");
+            $obj = new newArray($line);
+            $arrays[$obj->getName()] = $obj;
+        }
+
+        $x = stream_get_line(STDIN, 256 + 1, "\n");
+
+        $res = new Result($x, $arrays);
+
+        echo $res->findResult();
+    }
+
+    start();
+
+    // Write an answer using echo(). DON'T FORGET THE TRAILING \n
+    // To debug: error_log(var_export($var, true)); (equivalent to var_dump)
